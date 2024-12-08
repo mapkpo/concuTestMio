d2ed474976b3b1fa23da224d8d74122eca680808
Mijail <mijailpotinski@gmail.com>
2024-12-08 18:02:45 -0300

    deleted

--- src/main/Monitor.java
+++ src/main/Monitor.java
@@ -6,23 +6,23 @@ public class Monitor {
     final private Semaphore mutex;
   
     private boolean allInvariantsCompleted = false; //bandera para parar hilos
-    long startTime;
-    long endTime;
-   
 
-    public Monitor(Policy _politic){
+    public Monitor(Policy _policy){
         petri = new Rdp();
         mutex = new Semaphore(1, true);
-        startTime = System.currentTimeMillis();
     }
 
     /* Tries to acquire the mutex. */
-    private void getMutex() {
+    private void enterMonitor() {
         try{
             mutex.acquire();
         } catch (InterruptedException e){
             System.out.println("Monitor: interrupted while trying to acquire mutex: " + e);
         }
+    }
+
+    private void exitMonitor(){
+        mutex.release();
     }
 
 
@@ -39,8 +39,8 @@ public class Monitor {
     }
 
     public void finish(){
+        enterMonitor();
         allInvariantsCompleted = true;
-        getMutex();
         //System.out.println("Programa finalizado con: " + getBufferExported() + " invariantes");
         petri.printCounter();
         mutex.release();
